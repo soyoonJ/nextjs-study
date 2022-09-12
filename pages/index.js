bbd387024546b02263b29453176c40eb7b43af24
Soyoon Jeong <96245651+soyoonJ@users.noreply.github.com>
2022-09-12 23:06:49 +0900

    ✅ fetching data

--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,25 @@
 // useState 같은 훅 사용하려면 import 해와야 하긴 함
 // import {useState} from 'react';
 // import NavBar from "../components/NavBar";
+import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
+
+const API_KEY = "37a573fcd45b75bb928744debd503fb1";
 
 export default function Home() {
   // const [counter, setCounter] = useState(0)
+  const [movies, setMovies] = useState();
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      // console.log(results);
+      setMovies(results);
+    })();
+  }, []);
   return (
     <div>
       {/* <NavBar /> */}
@@ -22,9 +37,13 @@ export default function Home() {
           color: white;
         }
       `}</style> */}
-      <Seo title="Home">
-        <h1>Hello</h1>
-      </Seo>
+      <Seo title="Home" />
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
